4ecd563d088e46bb6f9bdc3727fa35944f9b7f35
王朝晖 <wangzhaohui@zhaotai.ren>
2018-11-07 09:47:07 +0800

    1

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,5 @@
 const SECURITY = '/freeapis-web-security/1/'
 
-
 const nickLogin = SECURITY + 'authentication/anonymous'//匿名用户登录
 
 module.exports = {
--- utils/network.js
+++ utils/network.js
@@ -115,23 +115,41 @@ function POST(url, params, success, fail) {
   })
 };
 
-function POST_LOGISTICS(params, success, fail) {
+/*
+展示进度条的网络请求
+url:网络请求的url
+params:请求参数
+success:成功的回调函数
+fail：失败的回调
+*/
+function PUT(url, params, success, fail) {
+
   let message = '数据加载中'
+
   console.log('requestURL:' + API_URL + url)
+
   wx.showLoading({
     title: message,
   })
   wx.request({
-    url: ReqURL,
+    url: API_URL + url,
     data: params,
-    header: {
-      // 'Content-Type': 'application/json'
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    method: 'POST',
+    // header: {
+    // 'Content-Type': 'application/json'
+    // 'content-type': 'application/x-www-form-urlencoded'
+    // },
+    method: 'PUT',
     success: function (res) {
+
+      // log
+      console.log(res.data)
+
       if (res.statusCode == 200) {
-        success(res.data)
+        if (res.data.code == '0000') {
+          success(res.data)
+        } else {
+          fail({ code: res.data.code, msg: res.data.msg })
+        }
       } else {
         wx.showToast({
           image: "/pages/images/icon_fail.png",
@@ -153,8 +171,64 @@ function POST_LOGISTICS(params, success, fail) {
   })
 };
 
+/*
+展示进度条的网络请求
+url:网络请求的url
+params:请求参数
+success:成功的回调函数
+fail：失败的回调
+*/
+function DELETE(url, params, success, fail) {
+
+  let message = '数据加载中'
+
+  console.log('requestURL:' + API_URL + url)
+
+  wx.showLoading({
+    title: message,
+  })
+  wx.request({
+    url: API_URL + url,
+    data: params,
+    // header: {
+    // 'Content-Type': 'application/json'
+    // 'content-type': 'application/x-www-form-urlencoded'
+    // },
+    method: 'DELETE',
+    success: function (res) {
+
+      // log
+      console.log(res.data)
+
+      if (res.statusCode == 200) {
+        if (res.data.code == '0000') {
+          success(res.data)
+        } else {
+          fail({ code: res.data.code, msg: res.data.msg })
+        }
+      } else {
+        wx.showToast({
+          image: "/pages/images/icon_fail.png",
+          title: "网络错误",
+        })
+        fail({ code: res.statusCode, msg: "网络错误" })
+      }
+    },
+    fail: function (res) {
+      wx.showToast({
+        image: "/pages/images/icon_fail.png",
+        title: "网络错误",
+      })
+      fail({ code: res.statusCode, msg: "网络错误" })
+    },
+    complete: function (res) {
+      wx.hideLoading()
+    }
+  })
+};
+
+
 module.exports = {
   GET: GET,
   POST: POST,
-  POST_LOGISTICS: POST_LOGISTICS
 }
