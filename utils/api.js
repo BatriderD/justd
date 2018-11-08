28784feac1bfbed1b9f6e2d94c41c4cc95e62752
王朝晖 <wangzhaohui@zhaotai.ren>
2018-11-08 17:45:21 +0800

    1

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,8 @@
 const SECURITY = '/freeapis-web-security/1/'
 
 const nickLogin = SECURITY + 'authentication/anonymous'//匿名用户登录
-const loginToken = SECURITY + 'authentication/public/authType/idType'//公网用户登录
+const loginToken = SECURITY + 'authentication/public/userNamePassword/mobile'//公网用户登录
+const agencyLogin = SECURITY + 'authentication/agency'//商家员工登录
 const getUserList = SECURITY + '我是用户列表接口'//获取列表
 const changeItem = SECURITY + '我是修改用户'//修改用户
 const delItem = SECURITY + '我是删除用户'//修改用户
@@ -10,6 +11,7 @@ const delItem = SECURITY + '我是删除用户'//修改用户
 module.exports = {
   nickLogin: nickLogin,
   loginToken: loginToken,
+  agencyLogin: agencyLogin,
   getUserList: getUserList,
   changeItem:changeItem,
   delItem: delItem,
